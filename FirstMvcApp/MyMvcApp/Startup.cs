c6db376a5fb1e20fcdc35ce6738ef62bbe7d12b9
chi <aznchimiesta@gmail.com>
2015-11-13 20:35:46 -0500

    new mvc proj

---
+++ FirstMvcApp/MyMvcApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyMvcApp.Startup))]
+namespace MyMvcApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
