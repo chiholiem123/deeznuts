c407b38e5626f7a8fb3bf8e883b70db99e45a9dc
chi <aznchimiesta@gmail.com>
2015-11-13 20:16:54 -0500

    New web page

---
+++ FirstMvcApp/ColorWebApp/Default.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ColorWebApp
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (this.RadioButtonList1.SelectedValue == "Red")
+            {
+                Response.Redirect("Red.aspx");
+            }
+            else if (this.RadioButtonList1.SelectedValue == "Blue")
+            {
+                Response.Redirect("Blue.aspx");
+            }
+            else
+                {
+                Response.Redirect("Green.aspx");
+            }
+        }
+    }
+}
